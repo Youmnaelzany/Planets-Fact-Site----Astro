607faa5c8d3be58806fe5259f18af1bf08869d22
Yaya <youmnael38@gmail.com>
2023-11-28 19:50:49 +0200

    Turning CSS style to Tailwind
    Turning CSS style to Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			'clr-White': ' #FFFFFF',
+			'clr-Iron': ' #d8d8d8',
+			'clr-Monsoon': ' #838391',
+			'clr-Martinique': ' #38384f',
+			'clr-Ebony': '#070724',
+			'clr-Pelorous': ' #419EBB',
+			'clr-Shamrock': ' #1ec1a2',
+			'clr-Royal-Blue': ' #2d68f0',
+			'clr-Purple-Heart': ' #6d2ed5',
+			'clr-Orange-Roughy': '#cd5120',
+			'clr-Valencia': ' #D14C32',
+			'clr-Punch': ' #d83a34',
+			'clr-Tan-Hide': ' #eda249',
+		},
+		fontFamily: {
+			'primary': ['Antonio', 'sans-serif'],
+			'secondary': ['League Spartan', 'sans-serif'],
+		},
+		extend: {
+		},
 	},
 	plugins: [],
 }
